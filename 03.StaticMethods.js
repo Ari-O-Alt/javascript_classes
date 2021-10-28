9c7f870a0bf381e6da79be85a5510590b2b068b0
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-10-28 11:04:27 +0200

    Added static methods documentation

--- 03.StaticMethods.js
+++ 03.StaticMethods.js
@@ -1,14 +1,17 @@
 // Satic methods are methods that can only be called on the class itself and not on an instance of that class
 // They are usefull when we build utilily classes (classes that group multiple helper methods together)
 
+// We create a class which will hold a bunch of calculations related methods
 class Calculations {
   constructor(firstNumber, secondNumber) {
     this.firstNumber = firstNumber;
     this.secondNumber = secondNumber;
   }
 
-  // here we're passing as arguments two instances created uisng the Calculations class
+  // To create a static method, we need to use the STATIC keyword
+  // Static methods needsto be places under the constructor
   static areResultsTheSame(firstPair, secondPair) {
+    // here we're passing as arguments two instances created uisng the Calculations class
     return firstPair.firstNumber + firstPair.secondNumber ===
       secondPair.firstNumber + secondPair.secondNumber
       ? true
